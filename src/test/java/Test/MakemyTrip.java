6cc2493cd4b4277413852c589a3b4b5bae105535
unknown <amitmeta122@gmail.com>
2024-03-18 21:48:10 +0530

    Test

--- src/test/java/Test/MakemyTrip.java
+++ src/test/java/Test/MakemyTrip.java
@@ -54,7 +54,7 @@ public class MakemyTrip extends BaseClass {
 		}
 
 		driver.findElement(By.xpath("//a[contains(text(),'Search')]")).click();
-		System.out.println("Test case passed fine");
+		System.out.println("Test case passed");
 
 	}
 
